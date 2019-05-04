2972ae21f9fec3a2dff09342cfa8e71b18fc3980
Wenyang Zhao <zhaowweny@gmail.com>
2019-05-04 12:17:59 +0800

    add eapi encrypt support

--- index.js
+++ index.js
@@ -7,6 +7,17 @@ var modulus =
     '00e0b509f6259df8642dbc35662901477df22677ec152b5ff68ace615bb7b725152b3ab17a876aea8a5aa76d2e417629ec4ee341f56135fccf695280104e0312ecbda92557c93870114af6c9d05c4f7f0c3685b7a46bee255932575cce10b424d813cfe4875d3e82047b97ddef52741d546b8e289dc6935b3ece0462db0a22b8e7';
 var nonce = '0CoJUm6Qyw8W8jud';
 var pubKey = '010001';
+var eapiKey = 'e82ckenh8dichen8';// eapi key
+var eapiHeader = {
+    "os": "osx",
+    "appver": "2.0.0",
+    "deviceId": "F7F5C832-AAA2-5F6C-B8EF-1E93418997EB%7CC07BA6B7-F6DC-4568-9ACB-CD9D93696C04",
+    "requestId": "43130936",
+    "clientSign": "",
+    "osver": "%E7%89%88%E6%9C%AC%2010.14.2%EF%BC%88%E7%89%88%E5%8F%B7%2018C54%EF%BC%89",
+    "batch-method": "GET",
+    "MUSIC_U": "90be07dfdedff92341c78118ea3344419eb4ece7111f30d4d305455650241e96d23b891712eeae35076e52ac8293123e1ec6c98d5a31c49ba70b41177f9edcea"
+};
 
 function createSecretKey(size) {
     var keys = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
@@ -41,7 +52,7 @@ function rsaEncrypt(text, pubKey, modulus) {
     // Math.pow(int(binascii.hexlify(text), 16), int(pubkey, 16), int(modulus, 16));
     return zfill(biRet.toString(16), 256);
 }
-
+// weapi encrypt
 function Encrypt(obj) {
     var text = JSON.stringify(obj);
     var secKey = createSecretKey(16);
@@ -53,6 +64,17 @@ function Encrypt(obj) {
     return {
         params: encText,
         encSecKey: encSecKey
+    }
+}
+// eapi encrypt
+function EAPI(path, object) {
+    var text = JSON.stringify(Object.assign(object, {e_r: 'false'}));
+    var message = 'nobody'+ path +'use'+ text +'md5forencrypt';
+    var digest = CryptoJS.MD5(message).toString();
+    var data = path +'-36cd479b6b5-'+ text +'-36cd479b6b5-'+ digest;
+    var params = encodeURIComponent(aesEncrypt(data, eapiKey, '', 'ECB').ciphertext.toString().toUpperCase());
+    return {
+        params: params
     }
 }
 
